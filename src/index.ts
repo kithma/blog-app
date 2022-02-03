3c842d9f11bb7838adfb22483351efdb55c2a88d
kmarindagoda@gmail.com <kmarindagoda@gmail.com>
2022-02-03 21:49:56 +0200

    add schemas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { ApolloServer } from "apollo-server";
 import { Query } from "./resolvers/index";
 import { typeDefs } from "./Schema";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export interface Context {
+	prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>
+}
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers: {
 		...Query
+	},
+	context: {
+		prisma
 	}
 });
 
